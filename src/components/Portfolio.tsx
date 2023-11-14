2e5f4a4ccba120289d503078eb8e2a551bcd55a1
ngGenerateUsername <mbenhamouda099@gmail.com>
2023-11-14 23:35:55 +0100

    prepare portfolio modal

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,7 +1,14 @@
-
-
+import { useState } from "react";
+import { Modal } from "react-responsive-modal";
+import 'react-responsive-modal/styles.css';
+import './ModalEdit.css';
 export default function Portfolio()
 {
+
+    const [open,setOpen] = useState(false);
+    
+    const onOpenModal = ()=>setOpen(true);
+    const onCloseModal = ()=>setOpen(false);
 
 
     return (
@@ -22,11 +29,26 @@ export default function Portfolio()
                     With its powerful combination of Flask for a robust backend, Flutter for an intuitive frontend, and PostgreSQL for secure data storage, */}
                     TaskFlow Pro delivers a seamless and efficient solution for supervisors and their teams.
                     </p>
-                    <a href="#" className="button button--flex button--small portfolio__button">
+                    <a href="#about" className="button button--flex button--small portfolio__button" onClick={onOpenModal}>
                         Details
-                        <i className="uil uil-newspaper button__icon"></i>
+                        <i className="uil uil-newspaper button__icon "></i>
                     </a>
+
+                    <Modal open={open} onClose={onCloseModal} center>
+                  
+                    <div className="services__modal-content" style={{margin:0,textAlign:"center"}}>
+                        {/* <h4 className="services__modal-title">UI/UX <br /> Designer</h4>
+                         */}
+                        <img src="img/TaskFlowApp.jpg" alt="" />
+                       
+                    </div>
+               
+                    </Modal>
+
+                 
                 </div>
+
+              
             </div>
     
         </div>
@@ -39,6 +61,7 @@ export default function Portfolio()
         
         <div className="swiper-pagination"></div>
     </div>
+
 </section>
 
     )
